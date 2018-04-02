86c355f8bcf3f7e48e4b98854d3169e7e1b6ea8e
jokingGao <fhhbbhc123@gmail.com>
2018-04-02 17:43:57 +0800

    task2 finished

--- Task2.py
+++ Task2.py
@@ -3,6 +3,9 @@
 你将在以后的课程中了解更多有关读取文件的知识。
 """
 import csv
+
+"""
+#The way of opening and reading csv files
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -10,6 +13,7 @@ with open('texts.csv', 'r') as f:
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
+"""
 
 """
 任务2: 哪个电话号码的通话总时间最长? 不要忘记，用于接听电话的时间也是通话时间的一部分。
@@ -23,3 +27,33 @@ September 2016.".
 如果键不存在于字典内，将此键加入字典，并将它的值设为给定值。
 """
 
+telTimeDict = {}
+telNum = -1
+longestTime = 0
+with open('calls.csv', 'r') as file:
+    reader = csv.reader(file)
+    calls = list(reader)
+    #print(calls)
+    for call in calls:
+        num1 = call[0]
+        num2 = call[1]
+        during = call[3]
+        
+        if telTimeDict.get(num1) == None:
+            telTimeDict[num1] = int(during)
+        else:
+            telTimeDict[num1] += int(during)
+        if telTimeDict.get(num2) == None:
+            telTimeDict[num2] = int(during)
+        else:
+            telTimeDict[num2] += int(during)
+            
+        if telTimeDict.get(num1) > longestTime:
+            telNum = num1
+            longestTime = telTimeDict.get(num1)
+        if telTimeDict.get(num2) > longestTime:
+            telNum = num2
+            longestTime = telTimeDict.get(num2)
+            
+print("{} spent the longest time, {} seconds, on the phone during September 2016."
+      .format(telNum, longestTime))
