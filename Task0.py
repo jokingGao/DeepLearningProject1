492badfb51304c23de9e9137e09720518d57ed70
jokingGao <fhhbbhc123@gmail.com>
2018-04-02 16:51:54 +0800

    task0 finished

--- Task0.py
+++ Task0.py
@@ -3,6 +3,10 @@
 你将在以后的课程中了解更多有关读取文件的知识。
 """
 import csv
+
+
+"""
+#The way of opening and reading csv files
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -10,6 +14,7 @@ with open('texts.csv', 'r') as f:
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
+"""
 
 
 """
@@ -20,3 +25,24 @@ with open('calls.csv', 'r') as f:
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
 
+with open('texts.csv', 'r') as file:
+    reader = csv.reader(file)
+    texts = list(reader)
+    #print(texts)
+    firstRecord = texts[0]
+    incomNum = firstRecord[0]
+    answerNum = firstRecord[1]
+    timeStamp = firstRecord[2]
+    print ("First record of texts, {incomNum} texts {answerNum} at time {timeStamp}"
+           .format(incomNum=incomNum, answerNum=answerNum, timeStamp=timeStamp))
+    
+with open('calls.csv', 'r') as file:
+    reader = csv.reader(file)
+    calls = list(reader)
+    lastRecord = calls[-1]
+    incomNum = lastRecord[0]
+    answerNum = lastRecord[1]
+    timeStamp = lastRecord[2]
+    during = lastRecord[3]
+    print("Last record of calls, {incomNum} calls {answerNum} at time {timeStamp}, lasting {during} seconds"
+          .format(incomNum=incomNum, answerNum=answerNum, timeStamp=timeStamp, during=during))
